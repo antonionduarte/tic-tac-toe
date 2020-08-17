8a67202736cd4366597ebb871233a23f76675360
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-08-17 03:49:34 +0100

    Added some hover stuff

--- index.js
+++ index.js
@@ -17,9 +17,26 @@ let l3r1_div = document.getElementById("l3r1");
 let l3r2_div = document.getElementById("l3r2");
 let l3r3_div = document.getElementById("l3r3");
 
-function game() {
+function game(userChoice) {
 
-    
+    console.log("userChoice");
 
 }
+
+function main() {
+    l1r1_div.addEventListener('click', () => game(11));
+    l1r2_div.addEventListener('click', () => game(12));
+    l1r3_div.addEventListener('click', () => game(13));
+
+    l2r1_div.addEventListener('click', () => game(21));
+    l2r2_div.addEventListener('click', () => game(22));
+    l2r3_div.addEventListener('click', () => game(23));
+
+    
+    l3r1_div.addEventListener('click', () => game(31));
+    l3r2_div.addEventListener('click', () => game(32));
+    l3r3_div.addEventListener('click', () => game(33));
+}
+
+main();
 

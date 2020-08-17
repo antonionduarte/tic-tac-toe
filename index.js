650808c59f4f962eae84b5fbdcdd17a357eca551
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2020-08-17 03:17:14 +0100

    Worked on the main HTML and CSS

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-// This is a comment!
-console.log("Hello world!");
+/** Variables */
+
+let player1_positions = [];
+let player2_positions = [];
+
+let player1 = true; // which player is currently playing
+
+/** DOM variables */
+
+let l1r1_div = document.getElementById("l1r1");
+let l1r2_div = document.getElementById("l1r2");
+let l1r3_div = document.getElementById("l1r3");
+let l2r1_div = document.getElementById("l2r1");
+let l2r2_div = document.getElementById("l2r2");
+let l2r3_div = document.getElementById("l2r3");
+let l3r1_div = document.getElementById("l3r1");
+let l3r2_div = document.getElementById("l3r2");
+let l3r3_div = document.getElementById("l3r3");
+
+function game() {
+
+    
+
+}
+
